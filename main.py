8012e633fd3e8f988d78fba20d58cac4212bbbfc
git <filip.mihelic@student.um.si>
2023-03-31 01:31:10 +0200

    Roberts filter

--- main.py
+++ main.py
@@ -11,3 +11,39 @@ def spremeni_kontrast(slika, alfa, beta):
     # Convert the adjusted NumPy array back to a 2D NumPy array and return it
     spremenjena_slika = spremenjena_slika.astype(np.uint8)
     return spremenjena_slika
+
+
+def my_roberts(slika):
+# Define the horizontal and vertical Roberts Cross kernels
+    r1 = np.array([[1,0],[0,-1]])
+    r2 = np.array([[0,1],[-1,0]])
+
+    # Get image size
+    vr, stolp = slika.shape[:2]
+
+    # Initialize output array
+    roberts_amplituda = np.zeros((vr, stolp))
+
+    # Compute the Roberts Cross edges
+    for i in range(1, vr):
+        for j in range(1, stolp):
+            r1o = np.sum(r1 * slika[i-1:i+1, j-1:j+1])
+            r2o = np.sum(r2 * slika[i-1:i+1, j-1:j+1])
+            roberts_amplituda[i,j] = np.sqrt(r1o**2 + r2o**2)
+
+    # Normalize the magnitude to [0, 1]
+    roberts_amplituda /= np.max(roberts_amplituda)
+
+    return roberts_amplituda
+
+
+img = cv2.imread("lenna.png",0) 
+cv2.imshow("Pokazi sliko ",img)
+
+#Roberts algorithm
+roberts = my_roberts(img)
+cv2.imshow("Primerjava roberts ", roberts)
+
+
+cv2.waitKey()
+cv2.destroyAllWindows()
